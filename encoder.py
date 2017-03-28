894a7649d4006f0cc03f03d0ef6f2046b76176a5
nnimish19 <nnimish19@gmail.com>
2017-03-28 19:08:26 -0400

    added RNN-encoder

--- encoder.py
+++ encoder.py
@@ -37,24 +37,23 @@ class RNNClassifier(object):
 # StochasticGradientDescent: learn the weights after each example. X contains one example
 #     Input: 1 sentence(English/French) context vector,
 #       @params:
-#       X np 2dmin array
-#           1st row: context vector for word 1 of Engligh sentence
-#           2nd row: context vector for word 2 of same English sentence
+#       X 2D np-array
+#           1st row: word_vector for word 1 of Engligh sentence
+#           2nd row: word_vector for word 2 of same English sentence
 #           ::
-#       y np 2dmin array
-#           1st row: context vector for word 1 of corresponding French sentence
-#          2nd row: context vector for word 2 of same French sentence
+#       Y 2D np-array
+#           1st row: word_vector for word 1 of corresponding French sentence
+#           2nd row: word_vector for word 2 of same French sentence
 #           ::
 #     Output:
 #       1. predicted sentence_vector in French. List of 1D np-array (word_vector for w1, w2 ...)
 #       2. Error in prediction. Numeric
-#       3. gradient on context vector (d(context_w1),d(context_w2)...). List of 1D np-array
 
-    def train(self, X, Y):
-        n, d = X.shape          # there are n words(n timestamps) having d features each. (d = size of 1 word vector in given language)
-        k = Y.shape[1]          # k = size of 1 word vector in target language. Note X = nxd, Y = nxk
+    def train(self, X, Decoder, Y):
+        n, d = X.shape              # n = words, d = size of 1 word_embedding/vector
+        k = 10 #Y.shape[1]          # k = size of context vector for one word
 
-        self.my_init(n,d,k)     #called just once
+        self.my_init(n,d,k)         #called just once
 
         dW2 = np.zeros_like(self.W2)
         dWh = np.zeros_like(self.Wh)
@@ -62,16 +61,14 @@ class RNNClassifier(object):
         db2 = np.zeros_like(self.b2)
         db1 = np.zeros_like(self.b1)
 
-        h = np.zeros((n+1, 1, self.hidden_dim))  #h[t] stores output hidden layer at time t(i.e., (t-1)th example). [[1,2,3...16]]
+        h = np.zeros((n+1, 1, self.hidden_dim))  #h[t] stores output of hidden layer at time t(i.e., i-th example). [[1,2,3...16]]
         output = []                              #stores output of output layer
-        total_error = 0
-        dX = []
 
         # Feed-forward--------------------------------
         for i in xrange(n):  # i= 0,1,2,..n-1    (w1 w2 w3)
             t=i+1
             Xi = np.array(X[i],ndmin=2)              # 1xd
-            yi = np.array(Y[i],ndmin=2)              # 1xk
+            # yi = np.array(Y[i],ndmin=2)            # 1xk
             # print Xi,yi
 
             net1 = np.dot(Xi, self.W1) + np.dot(h[t-1], self.Wh) + self.b1       #h[0]=0. X[i]=1xd, W=dxh
@@ -79,33 +76,41 @@ class RNNClassifier(object):
 
             net2 = np.dot(h[t], self.W2) + self.b2   # 1xh, hxk > 1xk. Eg: [[1,2,3]]
             scores = 1/(1+np.exp(-net2))
-            output.append(scores[0])
 
-            total_error += 0.5 * np.sum(np.square(yi - scores))
+            output.append(scores[0])    #Context vector (scores[0] has one dimension because we are training 1 example "row vector"(and not matrix) at a time)
 
-        scores = np.array(output)
+        ########################################################
+        # Now this output is the context vector that we'll pass to the Decoder
+        #     Get the dscore. from decoder
+        #     then compute dnet2
+
+        scores = np.array(output,ndmin=2)   #nxk (n: #words in X|  k: dimensions of each word in context vector)
+        pred_vector, error_in_pred, dscore = Decoder.train(scores, Y)       #predicted sentence embeddings > list
+        dscore = np.array(dscore)           #nxk (dscore[0] = derivative/gradient for ith node in output layer)
 
         # Backward--------------------------------
         dnet1_next_layer = np.zeros((1, self.hidden_dim))
         for t in xrange(n,0,-1):  # t = d,d-1,d-2,..1
             i=t-1
             Xi = np.array(X[i], ndmin=2)
-            yi = np.array(Y[i], ndmin=2)
+            # yi = np.array(Y[i], ndmin=2)
 
-            dscore = -(yi - scores[i])                      # 1xk,1xk
+            # dscore = -(yi - scores)
+            dnet2 = dscore[i] * (scores[i] * (1 - scores[i]))   #1xk, 1xk
+            dnet2 = np.array(dnet2,ndmin=2)
 
-            dnet2 = dscore * (scores[i] * (1 - scores[i]))  # 1xk
-            dW2 += np.dot(h[t].T, dnet2)                    # accumulate     hx1,1xk > hxk
-            db2 += np.sum(dnet2, axis=0, keepdims=True)     # accumulate
-            dht = np.dot(dnet2, self.W2.T) + np.dot(dnet1_next_layer, self.Wh.T)  # From_output_layer + h[t]_was_also_input_to_next_hidden_layer
-            #              1xk      (hxk).T                 1xh           hxh
+            dW2 += np.dot(h[t].T, dnet2)                    #accumulate     hx1,1xk > hxk
+            db2 += np.sum(dnet2, axis=0, keepdims=True)     #accumulate
+            dht = np.dot(dnet2, self.W2.T) + np.dot(dnet1_next_layer, self.Wh.T)    #From_output_layer + h[t]_was_also_input_to_next_hidden_layer
+                         #1xk    (hxk).T               1xh             (h,h)
 
             dnet1 = dht * (h[t] * (1-h[t]))                 # 1xh, 1xh
+
             dWh += np.dot(h[t - 1].T, dnet1)                # h[0] = 0
             dW1 += np.dot(Xi.T, dnet1)
             db1 += np.sum(dnet1, axis=0,keepdims=True)
 
-            dX.append(np.dot(dnet1, self.W1.T)[0])          #1xh, (dxh).T > 1xd
+            # dX.append(np.dot(dnet1, self.W1.T)[0])          #1xh, (dxh).T > 1xd  (Not required for encoder)
             dnet1_next_layer = dnet1
 
         # regularization gradient
@@ -119,9 +124,7 @@ class RNNClassifier(object):
         self.W1 += (self.step_size * (-dW1))
         self.b1 += (self.step_size * (-db1))
 
-        dX.reverse()    #reverse the order so it becomes d(w1),d(w2)...
-
-        return np.array(output), total_error, dX
+        return pred_vector, error_in_pred
 
     def predict(self, X):
         n, d = X.shape  # 2x1
---
+++ main.py
@@ -0,0 +1,70 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from gensim.models import Word2Vec as w2v
+import encoder as encoder
+import decoder as decoder
+
+Encoder = encoder.RNNClassifier()
+Decoder = decoder.RNNClassifier()
+
+def plot_graph(n, loss_axis):
+    # print loss_axis
+    plt.xlabel("# ith Example", fontsize=15)
+    plt.ylabel("Training Error", fontsize=15)
+    plt.plot(np.arange(n), loss_axis, '-')
+    plt.show()
+
+def sentenceToVector(model, s):     #Array of words
+    list= [model.wv[w] for w in s] #list of 2D np-array
+    return np.array(list)
+
+def vectorToSentence(model, V):    #Array of word_embeddings
+    s=""
+    for v in V:
+        sim = model.wv.most_similar(positive=[v], topn=1)
+        s+=sim[0][0]+" "
+    return s
+
+def trainScript(en_sentences,fr_sentences, en_model,fr_model):
+    loopcount = len(en_sentences)
+    loss_axis = []
+    for i in xrange(loopcount):
+        en_arr = en_sentences[i]
+        fr_arr = fr_sentences[i]
+        X = sentenceToVector(en_model, en_arr)
+        Y = sentenceToVector(fr_model, fr_arr)
+        # make X and Y to have same #words
+        if X.shape[0]<Y.shape[0]:
+            Y = Y[:X.shape[0]]
+        else:
+            X = X[:Y.shape[0]]
+
+        pred_vector, error = Encoder.train(X, Decoder, Y)
+        loss_axis.append(error)
+        print vectorToSentence(fr_model, pred_vector), "\n----------------\n"
+
+    plot_graph(loopcount, loss_axis)
+
+if __name__ == "__main__":
+    f= open("datasets/en_sample.txt","r")
+    text = f.read()
+    lines = text.split("\n")
+    en_sentences = [l.split(" ") for l in lines]
+    f.close()
+
+    f = open("datasets/fr_sample.txt", "r")
+    text = f.read()
+    lines = text.split("\n")
+    fr_sentences = [l.split(" ") for l in lines]
+    f.close()
+
+    # model = w2v([["line","one"], ["This", "is", "line", "two"]], size=2, window=2, min_count=1, workers=1)
+    en_model = w2v(en_sentences, size=5, window=5, min_count=1, workers=4)
+    fr_model = w2v(fr_sentences, size=5, window=5, min_count=1, workers=4)
+
+    # model.save(fname)
+    # en_model = w2v.load('datasets/english.bin')
+    # fr_model = w2v.load('datasets/french.bin')
+
+    # trainScript([en_sentences[0]], [fr_sentences[0]], en_model, fr_model)
+    trainScript(en_sentences, fr_sentences, en_model, fr_model)
